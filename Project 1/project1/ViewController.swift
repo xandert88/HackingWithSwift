224b9f4b4cd2fc3684222abd8e34ba749b3d44f1
xandert88 <50789735+xandert88@users.noreply.github.com>
2021-01-18 16:12:06 +1100

    Add files via upload
    Initial Commit

---
+++ Project 1/project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Alexander Thompson on 18/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
