7770dc8430bdf37a58e294dbe8b85919e515b36c
xandert88 <50789735+xandert88@users.noreply.github.com>
2021-02-03 12:32:22 +1100

    Add files via upload
    complete challenges

--- Project 10/Project 10/ViewController.swift
+++ Project 10/Project 10/ViewController.swift
@@ -41,9 +41,20 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     @objc func addNewPerson() {
         let picker = UIImagePickerController()
+        
+        let ac = UIAlertController(title: "", message: "Choose between camera or library.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Camera", style: .default){ _ in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) == true {
+                picker.sourceType = .camera
+                self.present(picker, animated: true)
+            }})
+        ac.addAction(UIAlertAction(title: "Library", style: .default){ _ in
+            self.present(picker, animated: true)
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         picker.allowsEditing = true
         picker.delegate = self
-        present(picker, animated: true)
+        present(ac, animated: true)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -68,8 +79,12 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
-        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        let ac = UIAlertController(title: "Rename or Delete Person", message: nil, preferredStyle: .alert)
         ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Delete", style: .destructive) { [weak self, weak ac] _ in
+            self?.people.remove(at: indexPath.item)
+            self?.collectionView.reloadData()
+        })
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
             guard let newName = ac?.textFields?[0].text else { return }
