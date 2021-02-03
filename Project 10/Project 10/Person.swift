9f164411548af32179fe42fca7de40c579bae1a8
xandert88 <50789735+xandert88@users.noreply.github.com>
2021-02-03 11:13:04 +1100

    Add files via upload
    project complete

---
+++ Project 10/Project 10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Alexander Thompson on 3/2/21.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project 10/Project 10/PersonCellCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCellCollectionViewCell.swift
+//  Project 10
+//
+//  Created by Alexander Thompson on 2/2/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
