f5f5d1c7455a98ae06c8c3685c6fa8bbf73ec5c4
Alex Thompson <AlexThompsonDevelopment@gmail.com>
2021-02-26 20:32:08 +1100

    Add files via upload
    complete project

---
+++ Project 16/Project 16/Capital.swift
@@ -0,0 +1,25 @@
+//
+//  Capital.swift
+//  Project 16
+//
+//  Created by Alexander Thompson on 26/2/21.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+    
+}
+
+
